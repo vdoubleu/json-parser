fa1f1cc9424177274af7d1fd82c15b115a96b8b9
Victor Wang <victorwang2001@gmail.com>
2019-10-30 11:09:22 -0400

    playing with result output

--- src/jsonParser.java
+++ src/jsonParser.java
@@ -1,25 +1,44 @@
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Map;
+
+import java.util.ArrayList;
 
 import com.google.gson.Gson;
 
 public class jsonParser {	
     public static void main(String[] ignored) throws Exception {
-
-        URL url = new URL("https://api.vexdb.io/v1/get_awards?team=7682&season=Toss%20Up");
-        InputStreamReader reader = new InputStreamReader(url.openStream());
-        Info response = new Gson().fromJson(reader, Info.class);
+    	int count = 1;
+    	
+        URL url;
+        matchInfo response;
         
-        System.out.println(response.status);
+        url = new URL("https://api.vexdb.io/v1/get_matches?limit_start=1&limit_number=1");
+        InputStreamReader reader = new InputStreamReader(url.openStream());
+        response = new Gson().fromJson(reader, matchInfo.class);
+        
         System.out.println(response.size);
- 
+        
+        
+        System.out.println(response.result);
+        
+        /*
+    	do{
+    		url = new URL("https://api.vexdb.io/v1/get_matches?limit_start=" + count + "&limit_number=1");
+            InputStreamReader reader = new InputStreamReader(url.openStream());
+            response = new Gson().fromJson(reader, matchInfo.class);
+            
+    		count++;
+    	}while(response.size != "0");*/
     }
 
-    private class Info {
-        //Map<String, String> status;
-        //Map<String, String> args;
-        String status;
+    private class matchInfo {
         String size;
+        ArrayList<matchResults> result;
+    }
+    
+    private class matchResults{
+    	String red1, red2, blue1, blue2;
+    	int redscore, bluescore;
+    	
     }
 }
