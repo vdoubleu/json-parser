77f37acb441d62c1a70da656df88b1f5757bdf54
Victor Wang <victorwang2001@gmail.com>
2019-11-07 17:52:31 -0500

    working on parser

--- src/jsonParser.java
+++ src/jsonParser.java
@@ -1,4 +1,6 @@
+import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.URL;
 
 import java.util.ArrayList;
@@ -8,35 +10,32 @@ import com.google.gson.Gson;
 public class jsonParser {	
     public static void main(String[] ignored) throws Exception {
     	int count = 1;
-    	
+    	  
         URL url;
         matchInfo response;
         
         url = new URL("https://api.vexdb.io/v1/get_matches?limit_start=1&limit_number=1");
-        InputStreamReader reader = new InputStreamReader(url.openStream());
-        response = new Gson().fromJson(reader, matchInfo.class);
+      InputStreamReader reader = new InputStreamReader(url.openStream());
+        /*response = new Gson().fromJson(reader, matchInfo.class);
         
-        System.out.println(response.size);
+        System.out.println(response.result[0]);
+        */
+    	
+    	Gson gson = new Gson();
+    	matchInfo[] myThings = gson.fromJson(reader, matchInfo[].class);
+    	
+        System.out.println(gson.toJson(myThings));
+       
         
-        
-        System.out.println(response.result);
-        
-        /*
-    	do{
-    		url = new URL("https://api.vexdb.io/v1/get_matches?limit_start=" + count + "&limit_number=1");
-            InputStreamReader reader = new InputStreamReader(url.openStream());
-            response = new Gson().fromJson(reader, matchInfo.class);
-            
-    		count++;
-    	}while(response.size != "0");*/
     }
 
     private class matchInfo {
         String size;
-        ArrayList<matchResults> result;
+        ArrayList<MatchResults> result;
+       
     }
     
-    private class matchResults{
+    private class MatchResults{
     	String red1, red2, blue1, blue2;
     	int redscore, bluescore;
     	
